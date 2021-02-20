fbb49294eb39adc7761a265dd7fd11438fb8eadb
haade <76404800+haade-administrator@users.noreply.github.com>
2021-02-20 19:30:47 +0100

    Update run.sh

--- frpc/run.sh
+++ frpc/run.sh
@@ -14,12 +14,18 @@ ADMIN_ADDR=$(jq --raw-output '.admin_addr' $CONFIG_PATH)
 ADMIN_PORT=$(jq --raw-output '.admin_port' $CONFIG_PATH)
 ADMIN_USER=$(jq --raw-output '.admin_user' $CONFIG_PATH)
 ADMIN_PWD=$(jq --raw-output '.admin_pwd' $CONFIG_PATH)
+PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
 FRPC_CONF=$FRP_PATH/frpc.ini
 
 if [ -f $FRPC_CONF ]; then
   rm $FRPC_CONF
+fi
+
+if [ ! $PROXY_NAME ]; then
+  PROXY_NAME=web
+  echo Using default proxy name $PROXY_NAME
 fi
 
 echo "[common]" >> $FRPC_CONF
@@ -31,8 +37,8 @@ echo "admin_addr = $ADMIN_ADDR" >> $FRPC_CONF
 echo "admin_port = $ADMIN_PORT" >> $FRPC_CONF
 echo "admin_user = $ADMIN_USER" >> $FRPC_CONF
 echo "admin_pwd = $ADMIN_PWD" >> $FRPC_CONF
-echo "<br/>"
-echo "[web]" >> $FRPC_CONF
+
+echo "[$PROXY_NAME]" >> $FRPC_CONF
 echo "type = http" >> $FRPC_CONF
 echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
