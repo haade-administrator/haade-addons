e58b257d984d486a73db1f33a01a8ce6f43b08a4
haade <76404800+haade-administrator@users.noreply.github.com>
2021-03-03 11:09:44 +0100

    Update run.sh

--- frpc/run.sh
+++ frpc/run.sh
@@ -18,7 +18,7 @@ HEALTH_CHECK_MAX_FAILED=$(jq --raw-output '.health_check_max_failed' $CONFIG_PAT
 HEALTH_CHECK_INTERVAL_S=$(jq --raw-output '.health_check_interval_s' $CONFIG_PATH)
 CUSTOM_DOMAINS=$(jq --raw-output '.custom_domains' $CONFIG_PATH)
 HTTP_NAME=$(jq --raw-output '.http_name // empty' $CONFIG_PATH)
-FRP_TYPE=$(jq --raw-output '.type' $CONFIG_PATH)
+HTTPS_NAME=$(jq --raw-output '.https_name // empty' $CONFIG_PATH)
 SERVER_CRT=$(jq --raw-output '.server_crt' $CONFIG_PATH)
 SERVER_KEY=$(jq --raw-output '.server_key' $CONFIG_PATH)
 PROXY_PROTOCOL_VERSION=$(jq --raw-output '.proxy_protocol_version' $CONFIG_PATH)
@@ -47,7 +47,7 @@ echo "authentication_method = token" >> $FRPC_CONF
 echo "token = $TOKEN_KEY" >> $FRPC_CONF
 \
 echo "[$HTTP_NAME]" >> $FRPC_CONF
-echo "type = $FRP_TYPE" >> $FRPC_CONF
+echo "type = http" >> $FRPC_CONF
 echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
 echo "use_encryption = $USE_ENCRYPTION" >> $FRPC_CONF
@@ -63,7 +63,7 @@ echo "group_key = $BALANCING_GROUP_KEY" >> $FRPC_CONF
 echo "custom_domains = $CUSTOM_DOMAINS" >> $FRPC_CONF
 
 echo "[$HTTPS_NAME]" >> $FRPC_CONF
-echo "type = $FRP_TYPE" >> $FRPC_CONF
+echo "type = https" >> $FRPC_CONF
 echo "local_port = 8123" >> $FRPC_CONF
 echo "custom_domains = nico.eu.domoxy.ovh" >> $FRPC_CONF
 echo "plugin = https2http" >> $FRPC_CONF
