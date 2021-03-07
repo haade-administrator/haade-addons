1b162c0ada26811fc1db75caaf1d1122e928e3dd
haade <76404800+haade-administrator@users.noreply.github.com>
2021-03-07 20:57:33 +0100

    Update run.sh

--- frpc/run.sh
+++ frpc/run.sh
@@ -13,16 +13,16 @@ USE_ENCRYPTION=$(jq --raw-output '.use_encryption' $CONFIG_PATH)
 USE_COMPRESSION=$(jq --raw-output '.use_compression' $CONFIG_PATH)
 BALANCING_GROUP=$(jq --raw-output '.balancing_group // empty' $CONFIG_PATH)
 BALANCING_GROUP_KEY=$(jq --raw-output '.balancing_group_key // empty' $CONFIG_PATH)
-HEALTH_CHECK_TYPE=$(jq --raw-output '.health_check_type' $CONFIG_PATH)
-HEALTH_CHECK_TIMEOUT_S=$(jq --raw-output '.health_check_timeout_s' $CONFIG_PATH)
-HEALTH_CHECK_MAX_FAILED=$(jq --raw-output '.health_check_max_failed' $CONFIG_PATH)
-HEALTH_CHECK_INTERVAL_S=$(jq --raw-output '.health_check_interval_s' $CONFIG_PATH)
+# HEALTH_CHECK_TYPE=$(jq --raw-output '.health_check_type' $CONFIG_PATH)
+# HEALTH_CHECK_TIMEOUT_S=$(jq --raw-output '.health_check_timeout_s' $CONFIG_PATH)
+# HEALTH_CHECK_MAX_FAILED=$(jq --raw-output '.health_check_max_failed' $CONFIG_PATH)
+# HEALTH_CHECK_INTERVAL_S=$(jq --raw-output '.health_check_interval_s' $CONFIG_PATH)
 CUSTOM_DOMAINS=$(jq --raw-output '.custom_domains' $CONFIG_PATH)
 CUSTOM_NAME=$(jq --raw-output '.custom_name' $CONFIG_PATH)
 SERVER_CRT=$(jq --raw-output '.server_crt' $CONFIG_PATH)
 SERVER_KEY=$(jq --raw-output '.server_key' $CONFIG_PATH)
 SSL_PHHR=$(jq --raw-output '.ssl_phhr' $CONFIG_PATH)
-PROXY_PROTOCOL_VERSION=$(jq --raw-output '.proxy_protocol_version // empty' $CONFIG_PATH)
+# PROXY_PROTOCOL_VERSION=$(jq --raw-output '.proxy_protocol_version // empty' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
 FRP_PATH_SSL=$FRP_PATH/ssl
@@ -76,20 +76,19 @@ echo "token = $TOKEN_KEY" >> $FRPC_CONF
 
 echo "[$CUSTOM_NAME]" >> $FRPC_CONF
 echo "type = https" >> $FRPC_CONF
-echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+#echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
 echo "use_encryption = $USE_ENCRYPTION" >> $FRPC_CONF
 echo "use_compression = $USE_COMPRESSION" >> $FRPC_CONF
 echo "custom_domains = $CUSTOM_DOMAINS" >> $FRPC_CONF
 # echo "group = $BALANCING_GROUP" >> $FRPC_CONF
 # echo "group_key = $BALANCING_GROUP_KEY" >> $FRPC_CONF
 # echo "plugin = https2http" >> $FRPC_CONF
-# echo "plugin_local_addr = $LOCAL_IP" >> $FRPC_CONF
-echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
-# echo "plugin_crt_path = $SERVER_CRT" >> $FRPC_CONF
-# echo "plugin_key_path = $SERVER_KEY" >> $FRPC_CONF
-# echo "plugin_host_header_rewrite = $SSL_PHHR" >> $FRPC_CONF
-# echo "plugin_header_X-From-Where = frp" >> $FRPC_CONF
-echo "proxy_protocol_version = $PROXY_PROTOCOL_VERSION" >> $FRPC_CONF
+echo "plugin_local_addr = $LOCAL_IP" >> $FRPC_CONF
+echo "plugin_crt_path = $SERVER_CRT" >> $FRPC_CONF
+echo "plugin_key_path = $SERVER_KEY" >> $FRPC_CONF
+echo "plugin_host_header_rewrite = $SSL_PHHR" >> $FRPC_CONF
+echo "plugin_header_X-From-Where = frp" >> $FRPC_CONF
+# echo "proxy_protocol_version = $PROXY_PROTOCOL_VERSION" >> $FRPC_CONF
 fi
 
 echo Start frp as client
