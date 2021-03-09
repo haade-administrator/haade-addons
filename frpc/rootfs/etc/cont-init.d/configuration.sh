dd1013fe9a9ac51551a69bb1286b2edd1b4086f1
haade <76404800+haade-administrator@users.noreply.github.com>
2021-03-09 16:29:26 +0100

    Update configuration.sh

--- frpc/rootfs/etc/cont-init.d/configuration.sh
+++ frpc/rootfs/etc/cont-init.d/configuration.sh
@@ -30,9 +30,6 @@ FRPC_CONF=$FRP_PATH/frpc.ini
 FRPC_CONF_CERT=$FRP_PATH_SSL/fullchain.pem
 FRPC_CONF_KEY=$FRP_PATH_SSL/privkey.pem
 
-CERTFILE=$(bashio::config 'certfile')
-KEYFILE=$(bashio::config 'keyfile')
-
 if [ -f $FRPC_CONF ]; then
   rm $FRPC_CONF
 fi
@@ -96,8 +93,8 @@ fi
 
 chmod 600 /var/frp/ssl/fullchain.pem
 chmod 600 /var/frp/ssl/privkey.pem
-cp "$FRPC_CONF_CERT" "/ssl/$CERTFILE"
-cp "$FRPC_CONF_KEY" "/ssl/$KEYFILE"
+cp "$FRPC_CONF_CERT" "/ssl/"
+cp "$FRPC_CONF_KEY" "/ssl/"
 
 echo Start frp as client
 
