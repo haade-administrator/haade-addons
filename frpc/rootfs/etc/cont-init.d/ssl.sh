f3436834e94d4402aa14c867ad19ab56c8275b50
haade <76404800+haade-administrator@users.noreply.github.com>
2021-03-10 09:15:35 +0100

    Update ssl.sh

--- frpc/rootfs/etc/cont-init.d/ssl.sh
+++ frpc/rootfs/etc/cont-init.d/ssl.sh
@@ -1,4 +1,11 @@
+FRP_PATH_SSL=$FRP_PATH/ssl
+FRPC_CONF=$FRP_PATH/frpc.ini
+FRPC_CONF_CERT=$FRP_PATH_SSL/fullchain.pem
+FRPC_CONF_KEY=$FRP_PATH_SSL/privkey.pem
+
 chmod 600 /var/frp/ssl/fullchain.pem
 chmod 600 /var/frp/ssl/privkey.pem
 cp "$FRPC_CONF_CERT" "/ssl/"
 cp "$FRPC_CONF_KEY" "/ssl/"
+
+echo "transfert certificat"
