3c31b989e337d4aaf83b1abde3edb26a64a3a516
@hannpet <hannpet@gmx.at>
2023-07-21 23:46:33 +0200

    modifications

--- README.md
+++ README.md
@@ -1,31 +1,6 @@
 <div align="center">
 <img src="images/logo.png">
-<h1>Home Assistant Add-on: Frpc</h1>
-
-[![GitHub release](https://img.shields.io/github/tag/fatedier/frp.svg?label=release-frp-original)](https://github.com/fatedier/frp/releases)
-[![GitHub release](https://img.shields.io/github/tag/haade-administrator/haade-addons.svg?label=release-frpc)](https://github.com/haade-administrator/haade-addons/releases)
-
-<br>
-<p>Run <a href="https://github.com/fatedier/frp">frp client</a> as a Home Assistant Add-on</p>
+<h2>Home Assistant Add-on: Frpc</h2>
 </div>
-
-## Installation
-
-Add the repository URL under **Supervisor → Add-on store → ⋮ → Manage add-on repositories**:
-
-    https://github.com/haade-administrator/haade-addons
-    
-The repository includes one add-ons for moment:
-
-frp client is a stable release that tracks the released versions of frp.
-
-    
-
-## What is frp?
-
-frp is a fast reverse proxy to help you expose a local server behind a NAT or firewall to the Internet. As of now, it supports **TCP** and **UDP**, as well as **HTTP** and **HTTPS** protocols, where requests can be forwarded to internal services by domain name.
-
-frp also has a P2P connect mode.
-    
-    
-Addons for hassio maintained by HAADE
+<br/>
+frp is a fast reverse proxy to help you expose a local server behind a NAT or firewall to the Internet. <br/> It supports **TCP** and **UDP**, as well as **HTTP** and **HTTPS** protocols, where requests can be forwarded to internal services by domain name.
--- frpc/run.sh
+++ frpc/run.sh
@@ -2,24 +2,11 @@
 set -e
 
 CONFIG_PATH=/data/options.json
-
 SERVER_ADDR=$(jq --raw-output '.server_addr' $CONFIG_PATH)
 SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
-FRP_TYPE=$(jq --raw-output '.frp_type' $CONFIG_PATH)
-PLUGINS=$(jq --raw-output '.plugins' $CONFIG_PATH)
-CUSTOM_NAME=$(jq --raw-output '.custom_name' $CONFIG_PATH)
 TOKEN_KEY=$(jq --raw-output '.token_key' $CONFIG_PATH)
-PROTOCOL=$(jq --raw-output '.protocol' $CONFIG_PATH)
-CONNECT_SERVER_LOCAL_IP=$(jq --raw-output '.connect_server_local_ip' $CONFIG_PATH)
-LOCAL_IP=$(jq --raw-output '.local_ip' $CONFIG_PATH)
 LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
-USE_ENCRYPTION=$(jq --raw-output '.use_encryption' $CONFIG_PATH)
-USE_COMPRESSION=$(jq --raw-output '.use_compression' $CONFIG_PATH)
-BALANCING_GROUP=$(jq --raw-output '.balancing_group' $CONFIG_PATH)
-BALANCING_GROUP_KEY=$(jq --raw-output '.balancing_group_key' $CONFIG_PATH)
-DOMAIN_PROTOCOL=$(jq --raw-output '.domain_protocol' $CONFIG_PATH)
-DOMAINS=$(jq --raw-output '.domains' $CONFIG_PATH)
-PROXY_PROTOCOL_VERSION=$(jq --raw-output '.proxy_protocol_version' $CONFIG_PATH)
+SUBDOMAIN=$(jq --raw-output '.subdomain' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
 FRPC_CONF=$FRP_PATH/frpc.ini
@@ -28,101 +15,17 @@ if [ -f $FRPC_CONF ]; then
   rm $FRPC_CONF
 fi
 
-if [ ! $CUSTOM_NAME ]; then
-  CUSTOM_NAME=frpconnect
-  echo Using default http name $CUSTOM_NAME
-fi
-
-if [ ! $BALANCING_GROUP ]; then
-  BALANCING_GROUP=frpweb
-  echo Using default balancing group name $BALANCING_GROUP
-fi
-
-if [ "$FRP_TYPE" = "http" ]; then
 echo "[common]" >> $FRPC_CONF
 echo "server_addr = $SERVER_ADDR" >> $FRPC_CONF
 echo "server_port = $SERVER_PORT" >> $FRPC_CONF
-
-if [ "$PLUGINS" = "none" ]; then
-echo "authentication_method = token" >> $FRPC_CONF
 echo "token = $TOKEN_KEY" >> $FRPC_CONF
-elif [ "$PLUGINS" = "fp-multiuser" ]; then
-echo "user = $CUSTOM_NAME" >> $FRPC_CONF
-echo "meta_token = $TOKEN_KEY" >> $FRPC_CONF
-fi
-
-echo "[$CUSTOM_NAME]" >> $FRPC_CONF
+echo "[homeassistant]" >> $FRPC_CONF
 echo "type = http" >> $FRPC_CONF
-echo "protocol = $PROTOCOL" >> $FRPC_CONF
-echo "connect_server_local_ip = $CONNECT_SERVER_LOCAL_IP" >> $FRPC_CONF
-echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
+echo "local_ip = 127.0.0.1" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
-echo "use_encryption = $USE_ENCRYPTION" >> $FRPC_CONF
-echo "use_compression = $USE_COMPRESSION" >> $FRPC_CONF
-echo "group = $BALANCING_GROUP" >> $FRPC_CONF
-echo "group_key = $BALANCING_GROUP_KEY" >> $FRPC_CONF
-if [ "$DOMAIN_PROTOCOL" = "custom_domains" ]; then
-echo "custom_domains = $DOMAINS" >> $FRPC_CONF
-elif [ "$DOMAIN_PROTOCOL" = "subdomain" ]; then
-echo "subdomain = $DOMAINS" >> $FRPC_CONF
-fi
-
-elif [ "$FRP_TYPE" = "https" ]; then
-echo "[common]" >> $FRPC_CONF
-echo "server_addr = $SERVER_ADDR" >> $FRPC_CONF
-echo "server_port = $SERVER_PORT" >> $FRPC_CONF
-
-if [ "$PLUGINS" = "none" ]; then
-echo "authentication_method = token" >> $FRPC_CONF
-echo "token = $TOKEN_KEY" >> $FRPC_CONF
-elif [ "$PLUGINS" = "fp-multiuser" ]; then
-echo "user = $CUSTOM_NAME" >> $FRPC_CONF
-echo "meta_token = $TOKEN_KEY" >> $FRPC_CONF
-fi
-
-echo "[$CUSTOM_NAME]" >> $FRPC_CONF
-echo "type = https" >> $FRPC_CONF
-echo "protocol = $PROTOCOL" >> $FRPC_CONF
-echo "connect_server_local_ip = $CONNECT_SERVER_LOCAL_IP" >> $FRPC_CONF
-echo "local_ip = $LOCAL_IP" >> $FRPC_CONF
-echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
-echo "use_encryption = $USE_ENCRYPTION" >> $FRPC_CONF
-echo "use_compression = $USE_COMPRESSION" >> $FRPC_CONF
-if [ "$DOMAIN_PROTOCOL" = "custom_domains" ]; then
-echo "custom_domains = $DOMAINS" >> $FRPC_CONF
-elif [ "$DOMAIN_PROTOCOL" = "subdomain" ]; then
-echo "subdomain = $DOMAINS" >> $FRPC_CONF
-fi
-echo "proxy_protocol_version = $PROXY_PROTOCOL_VERSION" >> $FRPC_CONF
-
-elif [ "$FRP_TYPE" = "http2https" ]; then
-echo "[common]" >> $FRPC_CONF
-echo "server_addr = $SERVER_ADDR" >> $FRPC_CONF
-echo "server_port = $SERVER_PORT" >> $FRPC_CONF
-
-if [ "$PLUGINS" = "none" ]; then
-echo "authentication_method = token" >> $FRPC_CONF
-echo "token = $TOKEN_KEY" >> $FRPC_CONF
-elif [ "$PLUGINS" = "fp-multiuser" ]; then
-echo "user = $CUSTOM_NAME" >> $FRPC_CONF
-echo "meta_token = $TOKEN_KEY" >> $FRPC_CONF
-fi
-
-echo "[$CUSTOM_NAME]" >> $FRPC_CONF
-echo "type = http" >> $FRPC_CONF
-echo "use_encryption = $USE_ENCRYPTION" >> $FRPC_CONF
-echo "use_compression = $USE_COMPRESSION" >> $FRPC_CONF
-if [ "$DOMAIN_PROTOCOL" = "custom_domains" ]; then
-echo "custom_domains = $DOMAINS" >> $FRPC_CONF
-elif [ "$DOMAIN_PROTOCOL" = "subdomain" ]; then
-echo "subdomain = $DOMAINS" >> $FRPC_CONF
-fi
-echo "plugin = http2https" >> $FRPC_CONF
-echo "plugin_local_addr = $LOCAL_IP:$LOCAL_PORT" >> $FRPC_CONF
-echo "plugin_host_header_rewrite = $LOCAL_IP" >> $FRPC_CONF
-echo "plugin_header_X-From-Where = frp" >> $FRPC_CONF
-fi
+echo "use_encryption = true" >> $FRPC_CONF
+echo "use_compression = false" >> $FRPC_CONF
+echo "subdomain = $SUBDOMAIN" >> $FRPC_CONF
 
 echo Start frp as client
-
 exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null
