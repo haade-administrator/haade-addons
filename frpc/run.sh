fa5d16d6598a6da77d420bf39e7a1769e4dbea70
@hannpet <hannpet@gmx.at>
2023-07-25 01:05:26 +0200

    fix: forgot to create ini file

--- frpc/run.sh
+++ frpc/run.sh
@@ -13,6 +13,20 @@ SERVER_PORT=$(bashio::config 'server_port')
 TOKEN_KEY=$(bashio::config 'token_key')
 LOCAL_PORT=$(bashio::config 'local_port')
 
+# Create ini-file
+echo "[common]" >> $FRPC_CONF
+echo "server_addr = $SERVER_ADDR" >> $FRPC_CONF
+echo "server_port = $SERVER_PORT" >> $FRPC_CONF
+echo "protocol = tcp" >>  $FRPC_CONF
+echo "token = $TOKEN_KEY" >> $FRPC_CONF
+echo "tls_enable = true" >> $FRPC_CONF
+echo "[homeassistant]" >> $FRPC_CONF
+echo "type = tcp" >> $FRPC_CONF
+echo "local_ip = 127.0.0.1" >> $FRPC_CONF
+echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+echo "use_encryption = true" >> $FRPC_CONF
+echo "use_compression = true" >> $FRPC_CONF
+
 # Start the client (respawn if it terminates)
 while true
 do
