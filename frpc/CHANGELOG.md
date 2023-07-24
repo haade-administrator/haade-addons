ad0abba591299e45667ffc74fe4475c654b142f1
@hannpet <hannpet@gmx.at>
2023-07-25 00:42:40 +0200

    1.3.0

--- frpc/CHANGELOG.md
+++ frpc/CHANGELOG.md
@@ -1,3 +1,5 @@
+## 1.3.0
+- respawn frps if it terminates
 ## 1.2.0
 - remove proto-parameter
 - clean documentation
--- frpc/run.sh
+++ frpc/run.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 set -e
-
-CONFIG_PATH=/data/options.json
-SERVER_ADDR=$(jq --raw-output '.server_addr' $CONFIG_PATH)
-SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
-TOKEN_KEY=$(jq --raw-output '.token_key' $CONFIG_PATH)
-LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
 FRPC_CONF=$FRP_PATH/frpc.ini
@@ -14,17 +8,24 @@ if [ -f $FRPC_CONF ]; then
   rm $FRPC_CONF
 fi
 
-echo "[common]" >> $FRPC_CONF
-echo "server_addr = $SERVER_ADDR" >> $FRPC_CONF
-echo "server_port = $SERVER_PORT" >> $FRPC_CONF
-echo "protocol = tcp" >>  $FRPC_CONF
-echo "token = $TOKEN_KEY" >> $FRPC_CONF
-echo "tls_enable = true" >> $FRPC_CONF
-echo "[hassio]" >> $FRPC_CONF
-echo "type = tcp" >> $FRPC_CONF
-echo "local_ip = 127.0.0.1" >> $FRPC_CONF
-echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
-echo "use_encryption = true" >> $FRPC_CONF
-echo "use_compression = true" >> $FRPC_CONF
+SERVER_ADDR=$(bashio::config 'server_addr')
+SERVER_PORT=$(bashio::config 'server_port')
+TOKEN_KEY=$(bashio::config 'token_key')
+LOCAL_PORT=$(bashio::config 'local_port')
 
-exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null
+# Start the client (respawn if it terminates)
+while true
+do
+  exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null &
+  PID=$!
+  bashio::log.info "Launched new frps-PID ${PID}"
+
+  while true
+  do
+    $(kill -0 ${PID} &> /dev/null) || break
+    sleep 5
+  done
+
+  bashio::log.info "${PID} has terminated."
+  sleep 5s
+done
